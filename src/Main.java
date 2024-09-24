b9462f863dd1cd559067c266b291d9c2ee2e7e12
jatur <turayjj@mail.uc.edu>
2024-09-24 15:30:54 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //maintinence costs for every season
+
+        double summerCost = 456.39;
+
+        double fallCost = 556.22;
+
+        double winterCost = 678.00;
+
+        double springCost = 338.56;
+
+        double totalYearlyCost = summerCost +fallCost +winterCost+ springCost;
+
+        System.out.println("total maintinence cost: " + totalYearlyCost);
     }
 }
